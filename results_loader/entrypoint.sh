3d521fa6a2acacbfb63fc479bae11c728a9cdfac
Nicholas McKee <nmsmckee@gmail.com>
2026-02-19 21:59:56 -0500

    Fix cron job for results again

--- results_loader/entrypoint.sh
+++ results_loader/entrypoint.sh
@@ -3,16 +3,21 @@
 set -e
 
 {
+    echo "export POSTGRES_HOST=${POSTGRES_HOST:-localhost}"
+    echo "export POSTGRES_DB=${POSTGRES_DB}"
+    echo "export POSTGRES_USER=${POSTGRES_USER}"
+    echo "export POSTGRES_PASSWORD=${POSTGRES_PASSWORD}"
+    echo "export POSTGRES_PORT=${POSTGRES_PORT:-5432}"
+    echo "export PYTHONUNBUFFERED=1"
+} > /etc/wca-env
+
+chmod 0600 /etc/wca-env
+
+{
     echo "SHELL=/bin/bash"
     echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-    echo "POSTGRES_HOST=${POSTGRES_HOST:-localhost}"
-    echo "POSTGRES_DB=${POSTGRES_DB}"
-    echo "POSTGRES_USER=${POSTGRES_USER}"
-    echo "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}"
-    echo "POSTGRES_PORT=${POSTGRES_PORT:-5432}"
-    echo "PYTHONUNBUFFERED=1"
     echo ""
-    echo "0 2 * * * root python3 /app/pull_results.py -s 2>&1 | logger -t wca-pull"
+    echo "0 2 * * * root source /etc/wca-env && python3 /app/pull_results.py 2>&1 | logger -t wca-pull"
 } > /etc/cron.d/wca-pull-results
 
 chmod 0644 /etc/cron.d/wca-pull-results
