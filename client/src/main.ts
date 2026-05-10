a2b913f8957b3a30215b4195b68ecd0960acad66
Nicholas McKee <nmsmckee@gmail.com>
2026-05-10 11:12:06 -0400

    Update deps, use ts 6

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,6 @@ import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import "@/style.css";
-import "@cubing/icons";
 
 const vueQueryPluginOptions: VueQueryPluginOptions = {
   queryClientConfig: {
